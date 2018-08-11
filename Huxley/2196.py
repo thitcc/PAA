b13e7d79f048e1a1b3e68e6e29b43ada45073659
Thiago Tenorio <ttcc@ic.ufal.br>
2018-08-11 10:24:27 -0300

    2196 almost

--- Huxley/2196.py
+++ Huxley/2196.py
@@ -1,4 +1,4 @@
-def upper_bound(array):
+def upper_bound(array, a):
     begin = 0
     end = len(array) - 1
     element = float((begin + end) / 2)
@@ -14,6 +14,17 @@ def upper_bound(array):
         else:
             begin = middle + 1
 
+    end = len(array) - 1 - index
+    total = 0
+
+    for index in range(end):
+        total += array[index] - element
+
+    if total == a:
+        print('%.4f' % element)
+    else:
+        upper_bound(array)
+
     return index
 
 
@@ -21,5 +32,12 @@ n, a = [int(x) for x in input().split()]
 while n != 0 and a != 0:
     height = [int(x) for x in input().split()]
     height.sort()
-    print(upper_bound(height))
+    total = sum(height)
+    if total == a:
+        print(":D")
+    elif total < a:
+        print("-.-")
+    else:
+        print('%.4f' % upper_bound(height, a))
+
     n, a = [int(x) for x in input().split()]
